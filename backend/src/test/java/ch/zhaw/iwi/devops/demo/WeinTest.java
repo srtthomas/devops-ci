c4ef5b3dba5960dd9123258a86577672694337e6
srtthomas <thomas.srt@gmail.com>
2024-05-15 13:08:19 +0200

    For presentation purpose remove unit tests

--- backend/src/test/java/ch/zhaw/iwi/devops/demo/WeinTest.java
+++ backend/src/test/java/ch/zhaw/iwi/devops/demo/WeinTest.java
@@ -4,40 +4,5 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class WeinTest {
-    @Test
-    void testGetAll() {
-        var testwein = new Wein(10,"Nebbiolo","Dunkler roter Wein");
-        assertEquals(10,testwein.getId());
-        assertEquals("Nebbiolo", testwein.getTitle());
-        assertEquals("Dunkler roter Wein", testwein.getDescription());
-
-    }
-
-    @Test
-    void testGetTitle() {
-        var testwein = new Wein(10,"Nebbiolo","Dunkler roter Wein");
-        assertEquals("Nebbiolo", testwein.getTitle());
-
-    }
-    @Test
-    void testGetDescription() {
-        var testwein = new Wein(10,"Nebbiolo","Dunkler roter Wein");
-        assertEquals("Dunkler roter Wein", testwein.getDescription());
-
-    }
-    @Test
-    void testSetId() {
-        var testwein = new Wein(10,"Nebbiolo","Dunkler roter Wein");
-        testwein.setId(20);
-        assertEquals(20, testwein.getId());
-    }
-
-    @Test
- void testciWein() {
- var wein = new Wein();
- wein.setId(42);
- assertEquals(null, wein.getTitle());
- assertEquals(null, wein.getDescription());
- assertEquals(42, wein.getId());
- }
+ 
 }
